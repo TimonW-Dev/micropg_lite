7cccf77f77945fe4d55d98258f75d27e6f06356a
TimonW-Dev <121683008+TimonW-Dev@users.noreply.github.com>
2023-09-29 18:05:10 +0200

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("pypi-README.md", "r") as fh:
 
 setup(
     name="micropg_lite", 
-    version="2.0.0",
+    version="2.1.0",
     author="TimonW-Dev",
     author_email="timon-github@outlook.com",
     description="A lightweight micropython PostgreSQL driver made for ESP8266 and other microchips with less ram",
