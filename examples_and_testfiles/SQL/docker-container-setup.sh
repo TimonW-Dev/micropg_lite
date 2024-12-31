d9b49dae765207b387ef74cab28fe5c00aed04c5
Timon W <121683008+TimonW-Dev@users.noreply.github.com>
2024-12-31 22:17:53 +0100

    fixed typo

--- examples_and_testfiles/SQL/docker-container-setup.sh
+++ examples_and_testfiles/SQL/docker-container-setup.sh
@@ -3,5 +3,5 @@
 # You may have to install docker. Debian example: sudo apt install docker.io
 # For a simple container it is sufficient to use the command on the line below and the data can be imported manually if necessary.
 # sudo docker run --restart always -itd -p 5432:5432 --name micropg_lite-testing -e POSTGRES_PASSWORD=123456 postgres
-docker build -t micropg_lite-fullpostgres .
+sudo docker build -t micropg_lite-fullpostgres .
 sudo docker run --restart always -itd -p 5432:5432 --name micropg_lite-testing micropg_lite-fullpostgres
