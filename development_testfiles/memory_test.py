88e12b4623f95ef7cea8775c7d22fa2ef1fed680
Timon W <121683008+TimonW-Dev@users.noreply.github.com>
2025-01-04 13:41:57 +0100

    Consumption comments removed as they no longer match the latest firmware.

--- development_testfiles/memory_test.py
+++ development_testfiles/memory_test.py
@@ -13,8 +13,3 @@ print("Free Memory After:                " + str(after) + " Bytes")
 
 difference = before - after
 print("The total usage of the script is: " + str(difference) + " Bytes")
-
-# micropg usage on pico w:       27456 Bytes
-# micropg_lite usage on pico w:  10576 Bytes
-# micropg_lite-V3 20.7.2024 usage pico w: 17440 Bytes
-# micropg_lite-V3 10.10.2024 usage pico w: 15296 Bytes
